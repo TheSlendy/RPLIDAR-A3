e1ecde758040321fa8e306da35e4d0e5465d3bd7
TheSlendy <48982621+TheSlendy@users.noreply.github.com>
2023-03-02 18:38:32 +0700

    Initialization commit

---
+++ Take_measurements/app/measure/main.cpp
@@ -0,0 +1,192 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>
+
+using namespace std;
+
+#include "rplidar.h"
+
+#ifndef _countof
+#define _countof(_Array) (int)(sizeof(_Array) / sizeof(_Array[0]))
+#endif
+
+
+#include <unistd.h>
+static inline void delay(_word_size_t ms){
+    while (ms>=1000){
+        usleep(1000*1000);
+        ms-=1000;
+    };
+    if (ms!=0)
+        usleep(ms*1000);
+}
+
+using namespace rp::standalone::rplidar;
+
+bool checkRPLIDARHealth(RPlidarDriver * drv)
+{
+    u_result     op_result;
+    rplidar_response_device_health_t healthinfo;
+
+
+    op_result = drv->getHealth(healthinfo);
+    if (IS_OK(op_result)) {
+        printf("RPLidar health status : %d\n", healthinfo.status);
+        if (healthinfo.status == RPLIDAR_STATUS_ERROR) {
+            fprintf(stderr, "Error, rplidar internal error detected. Please reboot the device to retry.\n");;
+            return false;
+        } else {
+            return true;
+        }
+
+    } else {
+        fprintf(stderr, "Error, cannot retrieve the lidar health code: %x\n", op_result);
+        return false;
+    }
+}
+
+#include <signal.h>
+bool ctrl_c_pressed;
+void ctrlc(int)
+{
+    ctrl_c_pressed = true;
+}
+
+int main(int argc, const char * argv[]) {
+    const char * opt_com_path = NULL;
+    _u32         baudrateArray[2] = {115200, 256000};
+    _u32         opt_com_baudrate = 0;
+    u_result     op_result;
+    chrono::steady_clock::time_point start_t;
+    chrono::steady_clock::time_point now_t;
+
+    bool useArgcBaudrate = false;
+    if (argc>1) opt_com_path = argv[1];
+
+    if (argc>2)
+    {
+        opt_com_baudrate = strtoul(argv[2], NULL, 10);
+        useArgcBaudrate = true;
+    }
+
+    if (!opt_com_path) {
+        opt_com_path = "/dev/ttyUSB0";
+    }
+
+	RPlidarDriver * drv = RPlidarDriver::CreateDriver(DRIVER_TYPE_SERIALPORT);
+    if (!drv) {
+        fprintf(stderr, "insufficent memory, exit\n");
+        exit(-2);
+    }
+    
+    rplidar_response_device_info_t devinfo;
+    bool connectSuccess = false;
+    if(useArgcBaudrate)
+    {
+        if(!drv)
+            drv = RPlidarDriver::CreateDriver(DRIVER_TYPE_SERIALPORT);
+        if (IS_OK(drv->connect(opt_com_path, opt_com_baudrate)))
+        {
+            op_result = drv->getDeviceInfo(devinfo);
+
+            if (IS_OK(op_result)) 
+            {
+                connectSuccess = true;
+            }
+            else
+            {
+                delete drv;
+                drv = NULL;
+            }
+        }
+    }
+    else
+    {
+        size_t baudRateArraySize = (sizeof(baudrateArray))/ (sizeof(baudrateArray[0]));
+        for(size_t i = 0; i < baudRateArraySize; ++i)
+        {
+            if(!drv)
+                drv = RPlidarDriver::CreateDriver(DRIVER_TYPE_SERIALPORT);
+            if(IS_OK(drv->connect(opt_com_path, baudrateArray[i])))
+            {
+                op_result = drv->getDeviceInfo(devinfo);
+
+                if (IS_OK(op_result)) 
+                {
+                    connectSuccess = true;
+                    break;
+                }
+                else
+                {
+                    delete drv;
+                    drv = NULL;
+                }
+            }
+        }
+    }
+    if (!connectSuccess) {
+        
+        fprintf(stderr, "Error, cannot bind to the specified serial port %s.\n"
+            , opt_com_path);
+        goto on_finished;
+    }
+
+    printf("RPLIDAR S/N: ");
+    for (int pos = 0; pos < 16 ;++pos) {
+        printf("%02X", devinfo.serialnum[pos]);
+    }
+
+    printf("\n"
+            "Firmware Ver: %d.%02d\n"
+            "Hardware Rev: %d\n"
+            , devinfo.firmware_version>>8
+            , devinfo.firmware_version & 0xFF
+            , (int)devinfo.hardware_version);
+
+    if (!checkRPLIDARHealth(drv)) {
+        goto on_finished;
+    }
+
+    signal(SIGINT, ctrlc);
+    
+    drv->startMotor();
+    drv->startScan(0,1);
+
+    start_t = chrono::steady_clock::now();
+	
+    while (1) {
+        rplidar_response_measurement_node_hq_t nodes[8192];
+        size_t   count = _countof(nodes);
+
+        op_result = drv->grabScanDataHq(nodes, count);
+
+        if (IS_OK(op_result)) {
+
+            drv->ascendScanData(nodes, count);
+
+	    now_t = chrono::steady_clock::now();
+			printf("Measure taken at %f\n", chrono::duration_cast<std::chrono::microseconds>(now_t - start_t).count()/1000000.0) ;
+
+            for (int pos = 0; pos < (int)count ; ++pos) {
+
+                if (nodes[pos].quality > 128) {
+                    printf("%03.2f, %08.2f\n", 
+                    (nodes[pos].angle_z_q14 * 90.f) / (1 << 14),
+                    ((float)nodes[pos].dist_mm_q2) / (1 << 2));
+
+                }
+            }
+        }
+
+        if (ctrl_c_pressed){ 
+            break;
+        }
+    }
+
+    drv->stop();
+    drv->stopMotor();
+on_finished:
+    RPlidarDriver::DisposeDriver(drv);
+    drv = NULL;
+    return 0;
+}
